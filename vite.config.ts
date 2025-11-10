ffb7f45c334668af28059b9a59c98deebf29c95a
TomaszB <tomasz.bracik@gmail.com>
2025-11-10 16:23:10 +0100

    Fix base path format in Vite config by adding trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -12,5 +11,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: '/rrso-calculator',
+  base: '/rrso-calculator/',
 });
